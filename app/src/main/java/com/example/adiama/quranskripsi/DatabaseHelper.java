204f6ffbed6e371985704711b8a9f41d9e7fd6e7
aadiama <aadiama>
2018-06-11 23:11:13 +0700

    maneh

--- app/src/main/java/com/example/adiama/quranskripsi/DatabaseHelper.java
+++ app/src/main/java/com/example/adiama/quranskripsi/DatabaseHelper.java
@@ -8,7 +8,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseHelper extends SQLiteAssetHelper {
     public static final String DATABASE_NAME = "wordbyword.db";
-    public static final int DATABASE_VERSION = 11;
+    public static final int DATABASE_VERSION = 12;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
