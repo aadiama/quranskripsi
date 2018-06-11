88eb85f5924b534baa294ea224c9c8b26951ea87
aadiama <aadiama>
2018-06-11 12:37:49 +0700

    maneh

--- app/src/main/java/com/example/adiama/quranskripsi/TableDialog1.java
+++ app/src/main/java/com/example/adiama/quranskripsi/TableDialog1.java
@@ -2,9 +2,11 @@ package com.example.adiama.quranskripsi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.SeekBar;
 
 public class TableDialog1 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
